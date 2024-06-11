e03d40ecc07cdd3a3c3ca6e8cc4167e13ce75ff9
kananmahmudov <kenan_mekanli@yahoo.com>
2024-06-11 10:47:23 -0400

    Web driver methods

--- src/test/java/io/loop/test/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/io/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -6,37 +6,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumTest {
     public static void main(String[] args) throws InterruptedException {
 
-        // Setting up the web driver
-        // WebDriverManager.chromedriver().setup(); but we do not need it with selenium 4.21
-        // if you use older version then you need dependency for webdrivermanager - boni garsia
-        //create an instance of webdriver
+        //  Setting up the web driver
+        //  WebDriverManager.chromedriver().setup(); but we do not need it with selenium 4.21
+        // if you use older version then you need dependency for webdrivermanager - bonin garsia
 
+        // create an instance pf webdriver
         WebDriver driver = new ChromeDriver();
 
         // maximize the window
         driver.manage().window().maximize();
-        // navigate to the web page
-        driver.get("https://www.loopcamp.io/");
-        Thread.sleep(5000);
+        //driver.manage().window().fullscreen();
 
+        // navigate to the web page
+        driver.get("https://www.loopcamp.io");
+        Thread.sleep(5000);
         driver.navigate().to("https://www.etsy.com");
 
-        //navigate back
+        // navigate back
         driver.navigate().back();
-
-        //navigate forward
+        // navigate forward
         driver.navigate().forward();
 
-        //refresh
+        // refresh
         driver.navigate().refresh();
 
-        //quit close
-        //driver.close();
+        // quit close
+        // driver.close();
         driver.quit();
+
     }
-
-
-
 
 
 }
